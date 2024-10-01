4efceb4e7cd0936f9e31bc985469ea3dd7f2427f
Jorash Robillos <jorashjonathan.robillos@cit.edu>
2024-10-01 22:27:35 +0800

    corrected users urlpattern

--- gitjob/urls.py
+++ gitjob/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.hero, name=""),
-    path('users/', include('users.urls')),
 ]
--- gitjobproject/urls.py
+++ gitjobproject/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('gitjob.urls')), 
+    path('users/', include('users.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
