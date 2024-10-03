a1b5031836c7abede70075286c8b627fb15f6809
Ranz Lumayno <ranz1211@gmail.com>
2024-10-03 11:12:57 +0800

    fixed home url in hero

--- gitjobproject/urls.py
+++ gitjobproject/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('gitjob.urls')), 
     path('users/', include('users.urls')),
-    path('jobs/', include('jobs.urls')),
+    # path('jobs/', include('jobs.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
