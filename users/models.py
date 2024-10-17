25832ad3bd34349867af96f992604ea1104cc763
Jorash Robillos <jorashjonathan.robillos@cit.edu>
2024-10-17 10:51:44 +0800

    register ui and kinda working profile

--- users/models.py
+++ users/models.py
@@ -1,10 +1,27 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+# class Skill(models.Model):
+#     string = models.CharField(max_length=100)
 
-# Create your models here.
+# class Experience(models.Model):
+#     string = models.CharField(max_length=100)
+
 class GitJobUser(AbstractUser):
-    birthdate = models.DateField('birthdate', blank=True, null=True)
+    job = models.CharField(max_length=100, blank=True, null=True)
+    job_company = models.CharField(max_length=100, blank=True, null=True)
+    home_address = models.CharField(max_length=100, blank=True, null=True)
+    phone_number = models.IntegerField(blank=True, null=True)
+    birthdate = models.DateField(blank=True, null=True)
     gender = models.CharField(max_length=20, blank=True, null=True)
+    description = models.TextField(max_length=500, blank=True, null=True)
+    # skills = models.ManyToManyField(Skill)
+    # experiences = models.ManyToManyField(Experience)
+    skills = models.JSONField(default=list)
+    experiences = models.JSONField(default=list)
 
     def __str__(self):
         return self.username
+
+class Resume(models.Model):
+    file = models.FileField(upload_to="pdf")
+    owner = models.ForeignKey(GitJobUser, on_delete=models.CASCADE)
--- users/views.py
+++ users/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth import get_user_model, authenticate ,login
-from .forms import LoginForm, RegisterForm
+from django.contrib.auth import authenticate ,login
+from django.http import HttpResponse
+from django.contrib.auth import get_user_model
+from .forms import LoginForm, RegisterForm, EditProfileForm
+from .models import GitJobUser
+import json
 
 # Create your views here.
 def login_view(request):
@@ -15,7 +19,7 @@ def login_view(request):
 
             if user_login is not None:
                 login(request, user_login)
-                return redirect('/jobs/home/')
+                return redirect('/home/')
             else:
                 messages.info(request, "Incorrect credentials")
     else:
@@ -35,30 +39,76 @@ def register_view(request):
 
             if password_input != retypepassword_input:
                 messages.info(request, "Passwords do NOT match")
-            else:
-                User = get_user_model()
-                if User.objects.filter(email=email_input).exists():
+            else:                
+                if GitJobUser.objects.filter(email=email_input).exists():
                     messages.info(request, "Email already used")
-                elif User.objects.filter(username=username_input).exists():
+                elif GitJobUser.objects.filter(username=username_input).exists():
                     messages.info(request, "Username already used")
                 else:
-                    user = User.objects.create_user(
+                    user = GitJobUser.objects.create(
                         email=email_input,
                         username=username_input,
-                        password=password_input,
                         first_name=fname_input,
                         last_name=lname_input,
                     )
+                    user.set_password(password_input)
                     user.save()
 
                     #auth the user after making account
                     user_login = authenticate(username=username_input, password=password_input)
                     if user_login is not None:
                         login(request, user_login)
-                        return redirect('/jobs/home/')
+                        return redirect('/home/')
                     else:
                         messages.info(request, "Error: Cannot log in registered account...")
 
     else:
         form = RegisterForm()
     return render(request, 'users/register.html', {"form": form})
+
+def profile_view(request):
+    if request.user.is_authenticated:
+        user = request.user
+
+        if user.skills:
+            skills_json = json.dumps(user.skills)
+        else:
+            skills_json = json.dumps([])
+        if user.experiences:
+            experiences_json = json.dumps(user.experiences)
+        else:
+            experiences_json = json.dumps([])
+
+        if request.method == "POST":
+            if request.POST.get('form_type') == 'edit_profile_form':
+                form = EditProfileForm(request.POST, instance=user)
+                if form.is_valid():
+                    form.save()
+                    return redirect('profile')
+            elif request.POST.get('form_type') == 'edit_skills_form':
+                skills_json_input = request.POST.get('skills_json')
+                try:
+                    skills_list = json.loads(skills_json_input)
+                except json.JSONDecodeError:
+                    skills_list = []
+                user.skills = skills_list
+                user.save()
+                form = EditProfileForm()
+                skills_json = json.dumps(user.skills)
+                return render(request, "users/profile.html", {"user": request.user, "form": form, "skills_json": skills_json, "experiences_json": experiences_json})
+            elif request.POST.get('form_type') == 'edit_experiences_form':
+                experiences_json_input = request.POST.get('experiences_json')
+                try:
+                    experiences_list = json.loads(experiences_json_input)
+                except json.JSONDecodeError:
+                    experiences_list = []
+                user.experiences = experiences_list
+                user.save()
+                form = EditProfileForm()
+                experiences_json = json.dumps(user.experiences)
+                return render(request, "users/profile.html", {"user": request.user, "form": form, "skills_json": skills_json, "experiences_json": experiences_json})
+        else:
+            form = EditProfileForm()
+            return render(request, "users/profile.html", {"user": request.user, "form": form, "skills_json": skills_json, "experiences_json": experiences_json})
+    else:
+        return HttpResponse("User not logged in")
