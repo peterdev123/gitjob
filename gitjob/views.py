f488bd4588e244b8f20b84cb97411b72f156086a
Jorash Robillos <jorashjonathan.robillos@cit.edu>
2024-10-27 02:12:09 +0800

    Robillos - temporary job postings UI

--- gitjob/views.py
+++ gitjob/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
+from manager.models import JobPost
 
 def hero(request):
     
     return render(request, 'gitjob/hero.html')
 
 def homepage(request):
-
-    return render(request, 'gitjob/homepage.html')
+    job_postings = JobPost.objects.all()
+    for job_posting in job_postings:
+        job_posting.tags = job_posting.tags.split(',')
+    return render(request, 'gitjob/homepage.html', {'job_postings': job_postings})
 
 def messages(request):
 
