00a2f7ec19becb3e04e3e76852e39817e8be1e01
Jorash Robillos <jorashjonathan.robillos@cit.edu>
2024-11-01 21:24:44 +0800

    (Robillos) temporary search users in messages.html

--- gitjob/views.py
+++ gitjob/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.db.models import Q
 from manager.models import JobPost
+from users.models import GitJobUser
 
 def hero(request):
     
@@ -12,5 +15,18 @@ def homepage(request):
     return render(request, 'gitjob/homepage.html', {'job_postings': job_postings})
 
 def messages(request):
-
+    if request.method == "POST" and request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        if request.POST.get('form_type') == 'search_user':
+            keyword = request.POST.get('input_keyword', '')
+            users = GitJobUser.objects.filter(
+                                    Q(first_name__icontains=keyword) |
+                                    Q(last_name__icontains=keyword) |
+                                    Q(username__icontains=keyword)
+            )
+            user_list = [{'username': user.username, 
+                          'profile_pic_url': user.profile_picture.url, 
+                          'first_name': user.first_name, 
+                          "last_name": user.last_name} 
+                        for user in users]
+            return JsonResponse({'users': user_list})
     return render(request, 'gitjob/messages.html')
